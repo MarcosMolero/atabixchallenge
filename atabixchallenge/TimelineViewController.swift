78357ec946ddd79a9f11c09e92e8dd397660606a
Marcos Molero <marcosmolero@MacBook-Pro-de-Marcos.local>
2017-03-07 18:51:05 +0100

    1.0.0 (1) - Muestro userID, userName, authToken, authTokenSecret. - Se crea TimelineViewController.

---
+++ atabixchallenge/TimelineViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TimelineViewController.swift
+//  atabixchallenge
+//
+//  Created by Marcos Molero on 7/3/17.
+//  Copyright © 2017 marcosmolero. All rights reserved.
+//
+
+import Foundation
+import TwitterKit
+
+class TimelineViewController: TWTRTimelineViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
--- atabixchallenge/ViewController.swift
+++ atabixchallenge/ViewController.swift
@@ -12,14 +12,19 @@ import TwitterKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
+    func draw() {
         let logInButton = TWTRLogInButton { (session, error) in
             if let unwrappedSession = session {
+                
+                print("\n\(unwrappedSession.userID)")
+                print("\n\(unwrappedSession.userName)")
+                print("\n\(unwrappedSession.authToken)")
+                print("\n\(unwrappedSession.authTokenSecret)")
+                
+                
                 let alert = UIAlertController(  title: "Logged In",
                                                 message: "User \(unwrappedSession.userName) has logged in",
-                                                preferredStyle: UIAlertControllerStyle.alert
+                    preferredStyle: UIAlertControllerStyle.alert
                 )
                 alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
                 self.present(alert, animated: true, completion: nil)
@@ -32,13 +37,17 @@ class ViewController: UIViewController {
         
         logInButton.center = self.view.center
         self.view.addSubview(logInButton)
-
-//        if let button == logInButton {
-//            button.center = self.view.center
-//            self.view.addSubview(button)
-//        }
-
         
+        //        if let button == logInButton {
+        //            button.center = self.view.center
+        //            self.view.addSubview(button)
+        //        }
+
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        draw()
     }
 
     override func didReceiveMemoryWarning() {
