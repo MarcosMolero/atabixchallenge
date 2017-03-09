736d70f1b0ae48e62b300666f905f63b7d76492c
Marcos <marcosmolero@mac-mini-de-marcos.local>
2017-03-09 08:26:27 +0100

    1.0.0 (1) - Se añade UserDefaults.

--- atabixchallenge/LoginViewController.swift
+++ atabixchallenge/LoginViewController.swift
@@ -20,8 +20,7 @@ class LoginViewController:UIViewController {
                 print("\n\(unwrappedSession.authToken)")
                 print("\n\(unwrappedSession.authTokenSecret)")
                 
-                
-                
+                self.saveToken(unwrappedSession.userID, unwrappedSession.userName, unwrappedSession.authToken, unwrappedSession.authTokenSecret)
             } else {
                 NSLog("Login error: %@", error!.localizedDescription);
             }
@@ -29,8 +28,16 @@ class LoginViewController:UIViewController {
         
         logInButton.center = self.view.center
         self.view.addSubview(logInButton)
-        
     }
+    
+    func saveToken(_ userID:String, _ userName:String, _ authToken:String, _ authTokenSecret:String) {
+        UserDefaults.standard.set(userID, forKey: "userID")
+        UserDefaults.standard.set(userName, forKey: "userName")
+        UserDefaults.standard.set(authToken, forKey: "authToken")
+        UserDefaults.standard.set(authTokenSecret, forKey: "authTokenSecret")
+        UserDefaults.standard.synchronize()
+    }
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
