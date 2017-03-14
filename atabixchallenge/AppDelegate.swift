3d377f6af7150aeda22e03e8d81b984862027fef
Marcos Molero <marcosmolero@MacBook-Pro-de-Marcos.local>
2017-03-14 17:42:05 +0100

    1.0.0 (1) - Clean code.

--- atabixchallenge/AppDelegate.swift
+++ atabixchallenge/AppDelegate.swift
@@ -17,7 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        
         Fabric.with([Twitter.self]).debug = true
         
         return true
